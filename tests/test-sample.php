56f294648ea8ec4c854d0ece5a57ad36306bda8e
gvolluz <gilles.volluz@gmail.com>
2016-08-19 14:53:17 +0200

    Configuring WP specific unit testing with PHPUnit and WP CLI

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Vesimeli
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
